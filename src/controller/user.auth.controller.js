dc735b0662c8c2fb38d69aab5bdbd59e46960f4f
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-01 23:52:25 +0600

    register controller

---
+++ src/controller/user.auth.controller.js
@@ -0,0 +1,35 @@
+import UserModel from "../Model/user.model.js";
+import bcrypt from 'bcryptjs'
+
+export const register = async (req, res) => {
+  try {
+
+    const { name, email, password } = req.body;
+
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const hashPass = await bcrypt.hashSync( password , 10)
+
+    const data={
+        name,
+        email,
+        password: hashPass
+    }
+    const newUser = new UserModel(data) 
+    await newUser.save();
+
+    res.status(201).json({
+      msg: "User Create Success",
+      error: false,
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
