a2474b5fa8fcce12f9b467fad90ad70345f1d38c
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-01 23:28:20 +0600

    productModel

---
+++ src/Model/product.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const preoductSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:Array,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    subCategory:{
+        type:String,
+        required:true
+    },
+    sizes:{
+        type:String,
+        required:true
+    },
+    populer:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Number,
+        required:true
+    },
+},{timestamps: true});
+
+const productModel = mongoose.model("product", preoductSchema);
+export default productModel;  //export the model
