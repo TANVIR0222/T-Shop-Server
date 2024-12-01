d1a5764fcd414ee18b095dcdfef0c7644bb68ef2
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-01 23:32:22 +0600

    user Model

---
+++ src/Model/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Provide Name"],
+    },
+    email: {
+      type: String,
+      require: [true, "Provide Email"],
+    },
+    password: {
+      type: String,
+      require: [true, "Provide Password"],
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+
+    role: {
+      type: String,
+      eunm: ["Admin", " USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+export default UserModel;
