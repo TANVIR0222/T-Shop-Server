33f58f5c8bf0627cc5f36f19c5ef271da59c46e1
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-02 21:53:42 +0600

    update login router

--- src/router/user.auth.route.js
+++ src/router/user.auth.route.js
@@ -4,7 +4,7 @@ import { login, logout, register } from '../controller/user.auth.controller.js';
 const userAuthRoute = Router()
 
 userAuthRoute.post('/register' ,register)
-userAuthRoute.get('/login' ,login)
+userAuthRoute.post('/login' ,login)
 userAuthRoute.post('/logout' ,logout)
 
 export default userAuthRoute;  //export the router to use in other files
