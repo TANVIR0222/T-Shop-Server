18179114f6241f497b66b780bf27dee17078daae
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-02 17:47:49 +0600

    delete Product

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -42,3 +42,22 @@ export const getAllProduct = async (req, res) => {
 
     }
 }
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const {id} = req.params;
+        if(!id){
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        const product = await productModel.findByIdAndDelete(id)
+        if(!product){
+            return res.status(404).json({ message: "Product not found" });
+        }
+        
+        res.status(200).json({ message: "Product deleted successfully" , success: true ,error:false});
+        
+    } catch (error) {
+        res.status(500).json({ msg: error.message || error, error: true, success: false }); 
+    }
+}
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -1,10 +1,11 @@
 import Router from 'express'
-import { addProduct, getAllProduct } from '../controller/product.controller.js';
+import { addProduct, deleteProduct, getAllProduct } from '../controller/product.controller.js';
 import upload from '../utils/multer.js';
 
 const productRoute = Router()
 
 productRoute.post('/add-product', upload.single('image') , addProduct)
 productRoute.get('/all-product', getAllProduct)
+productRoute.delete('/delete-product/:id', deleteProduct)
 
 export default productRoute;  
