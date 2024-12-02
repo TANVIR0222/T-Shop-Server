3294e37082c40c17beaf3e8c4f5c9ee43e4fad4e
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-02 22:24:51 +0600

    single User

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -19,3 +19,21 @@ export const updateRole = async (req, res) => {
    }
 
 }
+
+export const singleUser= async (req, res) => {
+
+    try {
+        const {id} = req.params;
+        if(!id){
+            return res.status(404).json({ message: "Id not found" });
+        }
+        const user = await UserModel.findById(id , 'id email role name').sort({createdAt : -1})
+        if(!user){
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ user, success: true, error: false });
+    } catch (error) {
+        res.status(500).json({ msg: error.message || error, error: true, success: false }); 
+
+    }
+}
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,8 +1,9 @@
 import Router from 'express'
-import { updateRole } from '../controller/user.controller.js';
+import { singleUser, updateRole } from '../controller/user.controller.js';
 
 const userRoute = Router()
 
 userRoute.patch('/update-role/:id', updateRole)
+userRoute.get('/single-user/:id', singleUser)
 
 export default userRoute; 
