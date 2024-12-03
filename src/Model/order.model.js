a55142a0c3729448c6626a0ecd8ef1b374a39d7e
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-04 00:12:42 +0600

    order Model

---
+++ src/Model/order.model.js
@@ -0,0 +1,39 @@
+const { default: mongoose } = require("mongoose");
+
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items:{
+        type: Array,
+        required: true
+    },
+    totalAmount:{
+        type: Number,
+        required: true
+    },
+    address:{
+        type: Object,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true,
+        // enum: ['pending', 'shipped', 'delivered', 'cancelled'],
+        default: 'Order Placed'
+    },
+    paymentMethod:{
+        type: String,
+        required: true
+    },
+    payment:{
+        type:Boolean ,
+        required: true,
+        default: false
+    }
+},{timestamps: true})
+const OrderModel = mongoose.model('Model', orderSchema);
+export default OrderModel;  //export the model to use in other files
