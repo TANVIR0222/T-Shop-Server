85f553e583b85918a1f2c8497441015f234123f9
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-04 22:46:07 +0600

    cart Router

--- src/router/cart.route.js
+++ src/router/cart.route.js
@@ -1,9 +1,11 @@
 import Router from 'express'
-import { addToCart, getAllCart } from '../controller/cart.controller.js';
+import { addToCart, deleteCart, getAllCart, singleCart } from '../controller/cart.controller.js';
 
 const cartRoute = Router();
 
 cartRoute.post('/add-cart' , addToCart)
 cartRoute.get('/all-cart' , getAllCart)
+cartRoute.delete('/delete-cart/:id' , deleteCart)
+cartRoute.get('/single-cart/:id' , singleCart)
 
 export default cartRoute;  //exportando o router para ser usado em outro arquivo
