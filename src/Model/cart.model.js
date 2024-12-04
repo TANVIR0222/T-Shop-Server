60154df319f939b0b0fa9a77de5ef604c818c2fe
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-04 16:24:49 +0600

    cart model

---
+++ src/Model/cart.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    image:{
+        type: Array,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    name:{
+        type: String ,
+        required: true
+    },
+},{timestamps: true})
+const cartModel = mongoose.model('Cart', cartSchema);
+export default cartModel;  //export the model to use in other files
