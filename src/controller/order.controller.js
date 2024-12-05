5b604d8aeb5f31c0ca1dea84367efcf723ec2b57
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-05 12:43:01 +0600

    placeOrder

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -1,25 +1,20 @@
 import OrderModel from "../Model/order.model.js";
-import UserModel from "../Model/user.model.js";
 
 export const placeOrder = async(req,res) => {
   try {
-    const {userId, item , address , totalAmount } = req.body;
-
+    const {userId, items , address , totalAmount } =  req.body;    
     const orderData = {
         userId,
-        item,
+        items,
         address,
         totalAmount,
         paymentMethod: 'usd',
         payment: false ,
     }
 
-    const newOrder = await OrderModel.create(orderData);
+    const newOrder = await OrderModel(orderData);
     await newOrder.save();
-
-    // await UserModel.findByIdAndUpdate(userId , {carData : {} })
-
-    res.status(201).json({ message: "order successfully" , success: true , error:false });
+    res.status(201).json({ message: "order successfully" , success: true , error:false , newOrder});
 
   } catch (error) {
     res.status(500).json({ msg: error.message || error, error: true, success: false });
--- src/router/order.route.js
+++ src/router/order.route.js
@@ -1,7 +1,16 @@
 import Router from 'express'
+import { allOrders, placeOrder, placeOrderStripe, updateStatus } from '../controller/order.controller.js';
 
 const orderRoute = Router()
 
-orderRoute.post('/')
+orderRoute.post('/list' , allOrders)
+orderRoute.post('/status' , updateStatus)
+
+// for Payment
+orderRoute.post('/place' , placeOrder )
+orderRoute.post('/stripe' , placeOrderStripe )
+
+// for user 
+orderRoute.post('/userorders' , placeOrderStripe )
 
 export default orderRoute;  //exportando o router para ser usado em outro arquivo
